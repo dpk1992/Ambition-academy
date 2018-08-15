93e2b09083eb9cc846876df1166d539b0df5891b
deepak kumar <dpk120@gmail.com>
2018-08-15 16:24:42 +0530

    interface themes

--- ambition/src/main/java/com/ambition/controller/PageController.java
+++ ambition/src/main/java/com/ambition/controller/PageController.java
@@ -1,7 +1,6 @@
 package com.ambition.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -11,10 +10,34 @@ public class PageController {
 	@RequestMapping(value = { "/", "/home", "/index" })
 	public ModelAndView index() {
 		ModelAndView mav = new ModelAndView("page");
-		mav.addObject("greeting", "welcome to spring world");
+		mav.addObject("title","Home");
+		mav.addObject("userClickHome",true);
 		return mav;
 
 	}
+	@RequestMapping(value = "/about" )
+	public ModelAndView about() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title","about");
+		mav.addObject("userClickAbout",true);
+		return mav;
 
+	}
+	@RequestMapping(value = "/contact")
+	public ModelAndView contact() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title","contact");
+		mav.addObject("userClickContact",true);
+		return mav;
+
+	}
+	@RequestMapping(value = "/product")
+	public ModelAndView product() {
+		ModelAndView mav = new ModelAndView("page");
+		mav.addObject("title","product");
+		mav.addObject("userClickProduct",true);
+		return mav;
+
+	}
 
 }
